14170b46c1059a98c140c2cc9806fefdf94ed29b
KevinArce <iav.kevinarce@ufg.edu.sv>
2023-02-06 11:47:08 -0600

    first commit

---
+++ frontend/src/AuthPage.jsx
@@ -0,0 +1,38 @@
+import axios from "axios";
+
+const AuthPage = (props) => {
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const { value } = e.target[0];
+    axios
+      .post("http://localhost:3001/signup", {
+        username: value,
+        secret: value,
+      })
+      .then((r) => props.onAuth({ ...r.data, secret: value }))
+      .catch((e) => console.log("Auth Error", e));
+  };
+
+  return (
+    <div className="background">
+      <form onSubmit={onSubmit} className="form-card">
+        <div className="form-title">Welcome 👋</div>
+
+        <div className="form-subtitle">Set a username to get started</div>
+
+        <div className="auth">
+          <div className="auth-label">Username</div>
+          <input className="auth-input" name="username" />
+          <div className="auth-label">Secret</div>
+          <input className="auth-input" name="secret" />
+          
+          <button className="auth-button" type="submit">
+            Enter
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default AuthPage;
